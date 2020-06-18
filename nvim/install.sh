a165ef1fa5f83a78f88e2b819271f8816f296e96
Joshua Wu <joshua.wu.0910@gmail.com>
2020-06-17 17:28:52 -0700

    Add nvim configs

---
+++ nvim/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+APP="nvim"
+FILE_NAME="config"
+CONFIG_DIR="$HOME/.config"
+DOTFILE_DIR="$HOME/dotfiles"
+
+DOTFILE_APP_DIR="$DOTFILE_DIR/$APP"
+
+[ ! -d "$CONFIG_DIR/$APP" ] && mkdir "$CONFIG_DIR/$APP"
+
+if [[ -L "$CONFIG_DIR/$APP/$FILE_NAME" || -f "$CONFIG_DIR/$APP/$FILE_NAME" ]]; then
+    rm $CONFIG_DIR/$APP/$FILE_NAME &&
+    ln -s $DOTFILE_APP_DIR/$FILE_NAME $CONFIG_DIR/$APP/$FILE_NAME
+else
+    ln -s $DOTFILE_APP_DIR/$FILE_NAME $CONFIG_DIR/$APP/$FILE_NAME
+fi
+
